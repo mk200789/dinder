32de89160d05df16b7ee975274a3d82283a95464
Kim Mok <cafe.mui@gmail.com>
2017-05-07 22:32:19 -0400

    created appcontainer

---
+++ app/AppContainer.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+export default class AppContainer extends Component{
+	constructor(props){
+		super(props)
+		this.state = {
+			store: {},
+			theme: null
+		}
+	}
+
+	render(){
+		return null
+	}
+}
