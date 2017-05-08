61ce431b2180adf4d5778622806919053ad3edef
Kim Mok <cafe.mui@gmail.com>
2017-05-07 22:39:42 -0400

    add basic drawer component

--- app/AppContainer.js
+++ app/AppContainer.js
@@ -1,15 +1,40 @@
 import React, { Component } from 'react'
 
+import { Drawer, View } from 'native-base'
+
 export default class AppContainer extends Component{
 	constructor(props){
 		super(props)
 		this.state = {
+			toggled: false,
 			store: {},
 			theme: null
 		}
 	}
 
+	toggleDrawer(){
+		//check if the draw is currently open or not
+		this.state.toggled ? this._drawer.close() : this._drawer.open()
+	}
+
+	openDrawer(){
+		this.setState({toggled: true})
+	}
+
+	closeDrawer(){
+		this.setState({toggled: false})
+	}
+
 	render(){
-		return null
+		return (
+			<Drawer
+				ref = { (ref) => this._drawer = ref}
+				type = "displace"
+				content = { <View style={{backgroundColor: "#000", height: 1000}} />}
+				onClose = {this.closeDrawer.bind(this)}
+				onOpen = {this.openDrawer.bind(this)}
+				openDrawerOffset = {0.2}
+			></Drawer>
+		)
 	}
 }
