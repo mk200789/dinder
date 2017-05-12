7f7e93d8d565628a51c28889dbd1a20864535e5e
Kim Mok <cafe.mui@gmail.com>
2017-05-11 22:23:09 -0400

    create an empty loginscene and authentication store

---
+++ stores/authStore.js
@@ -0,0 +1,22 @@
+import {observable, action}  from 'mobx'
+import firebase from 'firebase'
+
+export default class AuthStore {
+  @observable autUser = null
+
+  constructor(){
+    firebase.auth().onAuthStateChanged((user) => {
+      this.authUser = user;
+    });
+  }
+
+  @action
+  signIn({email, password}){
+    if (this.authUser){
+      return Promise.resolve(this.authUser)
+    }
+    return firebase.auth().signInWithEmailAndPassword(email, password);
+  }
+
+  
+}
